039a50d281f12842c666581b23783d43f9d1f947
Ozan <oznkucuk@gmail.com>
2023-02-26 02:12:18 +0300

    Login uygulamasi
    programda kayitli kullanıcı adı şifresi ile login uygulamasi. Döngü ve koşul durumu aliştirmasi

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace login_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kullanıcı Giriş Ekranına Hoşgeldiniz.");
+            string kAdi = "ozan";
+            string kSifresi = "123456";
+
+            int hak =0;
+            while (hak<3)
+            {
+            Console.Write("Kullanıcı Adı : ");
+            string gAdi = Console.ReadLine();
+            Console.Write("Kullanıcı Şifresi : ");
+            string gSifresi = Console.ReadLine();
+            
+                if (kAdi==gAdi && kSifresi==gSifresi)
+                {
+                    Console.WriteLine("Başarı ile giriş yapıldı..");
+                    break;
+                }
+                else if(kAdi!=gAdi || kSifresi!=gSifresi)
+                {
+                    Console.WriteLine("Kullanıcı adı yada şifre hatalı!!!");
+                    hak +=1;
+                    if (hak==3)
+                    {
+                        Console.WriteLine("Giriş hakkınız dolmuştur!");
+                    }
+                }
+            }
+        }
+    }
+}
